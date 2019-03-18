832b4584d4896d29a5a3edae5b60d8d3bc32e0cc
Konrad Kleine <kkleine@redhat.com>
2019-03-18 18:05:25 +0100

    Add main_test.go

---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/redhat-developer/app-service/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMainFunc(t *testing.T) {
+	os.Args = []string{
+		"noop",
+	}
+
+	// If we need to set a config variable
+	restoreFunc := testutils.UnsetEnvVarAndRestore("foo")
+	defer restoreFunc()
+
+	go main()
+
+	// GET /status?format=yaml
+	for i := 0; i < 100; i++ {
+		resp, err := http.Get("http://0.0.0.0:8080/status?format=yaml")
+		if err == nil && resp != nil && resp.Body != nil {
+			defer resp.Body.Close()
+			break
+		}
+	}
+
+	// send SIGTERM to current process to trigger server shutdown
+	pid := os.Getpid()
+	err := syscall.Kill(pid, syscall.SIGTERM)
+	require.NoError(t, err)
+}
